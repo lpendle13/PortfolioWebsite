8a15e570addb6ba0b194f36ed2abba3fa5c81b9a
lpendle13 <89271601+lpendle13@users.noreply.github.com>
2024-04-12 21:49:50 -0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 function toggleList(x) {
   x.classList.toggle('menu-close-icon');
   document.getElementById('hamburger-menu').classList.toggle('open');
+  document.getElementById('menu-btn-insert').innerHTML = " <img src="/images/xmark-solid.svg;"> ";
 }
 
  
